b6dfc4e67d5d720c846a1a473ade17d4fcc891f0
Shashank Shrivastav <shashank.shrivastav@abcplusd.com>
2019-10-11 08:03:58 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My App"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text("Unchanged Text"),
+          RaisedButton(
+            child: Text("Change"),
+            onPressed: null,
+          )
+        ],
+      ),
+    );
+  }
+}
